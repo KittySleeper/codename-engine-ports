9fa21b6fbf614fa4ccbd78d002e1fe79b2ceafca
team-504 <75187806+team-504@users.noreply.github.com>
2022-11-08 22:11:51 -0600

    i like tweens

--- data/charts/for-hire/script.hx
+++ data/charts/for-hire/script.hx
@@ -4,5 +4,9 @@ function stepHit(curStep){
         FlxTween.tween(FlxG.camera, {zoom: 0.7}, 1.5);
         case 20:
         defaultCamZoom = 0.7;
+        case 272:
+        FlxTween.tween(FlxG.camera, {zoom: 0.5}, 8.5);
+        case 336:
+        FlxTween.tween(FlxG.camera, {zoom: 0.7}, 8.5);
     }
 }
