9595ccfac8b85b8da75da003f471e6d225a5c286
team-504 <75187806+team-504@users.noreply.github.com>
2022-12-10 20:50:04 -0600

    delete

--- bside/data/charts/edit.hx
+++
@@ -1,9 +0,0 @@
-import funkin.system.MusicBeatState;
-
-function update() {
-	if (FlxG.keys.justPressed.SIX) {
-		var state = new MusicBeatState(true, "CharEdit");
-		state.stateScript.set("daAnim", dad.curCharacter);
-		FlxG.switchState(state);
-	}
-}
